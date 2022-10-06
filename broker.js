f6fda3e5952b10663dd06b94e6a4101aa32bad58
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2022-10-06 03:35:20 -0500

    add

--- broker.js
+++ broker.js
@@ -1,91 +1,112 @@
-const aedes = require("aedes")();
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const httpServer = require("http").createServer();
-const mqtt = require("mqtt");
-const Item = require("./models/Item");
-const server = require("net").createServer(aedes.handle);
-const ws = require("ws");
+// const aedes = require("aedes")();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const app = express();
+// const httpServer = require("http").createServer();
+// const mqtt = require("mqtt");
+// const Item = require("./models/Item");
+// const server = require("net").createServer(aedes.handle);
+// const ws = require("ws");
 
-const PORTLISTEN = process.env.PORT || 3000;
-const portWs = 8000;
-const port = 1883;
+// const PORTLISTEN = process.env.PORT || 3000;
+// const portWs = 8000;
+// const port = 1883;
 
-const portMQ = 8888;
-const { mongoUrl, cookieKey } = require("./keys");
-const cors = require("./middleware/cors.middleware");
-const client = mqtt.connect("mqtt://192.168.100.8:1883");
-const topic = "test123";
+// const portMQ = 8888;
+// const { mongoUrl, cookieKey } = require("./keys");
+// const cors = require("./middleware/cors.middleware");
+// const client = mqtt.connect("mqtt://192.168.100.8:1883");
+// const topic = "test123";
 
-app.use(cors);
-let socket;
-// wss.on("connection", function connection(ws) {
-//   socket = ws;
+// app.use(cors);
+// let socket;
+// // wss.on("connection", function connection(ws) {
+// //   socket = ws;
+// // });
+
+// client.on("message", (topic, message) => {
+//   message = message.toString();
+//   const item = new Item({ mess: message });
+//   item.save();
+//   if (socket) {
+//     socket.send(message);
+//   }
 // });
 
-client.on("message", (topic, message) => {
-  message = message.toString();
-  const item = new Item({ mess: message });
-  item.save();
-  if (socket) {
-    socket.send(message);
-  }
+// client.on("connect", () => {
+//   client.subscribe(topic);
+// });
+// const wss = new ws.Server(
+//   {
+//     port: 8000,
+//   },
+//   () => console.log("serverStarted")
+// );
+// wss.on("connection", (ws) => {
+//   ws.on("message", (message) => {
+//     message = JSON.parse(message);
+//     console.log(message);
+//     wss.clients.forEach((client) => {
+//       client.send(
+//         JSON.stringify({
+//           ...message,
+//           new: true,
+//           id: Date.now(),
+//           time: Date.now(),
+//         })
+//       );
+//     });
+//   });
+// });
+// const start = async () => {
+//   try {
+//     await mongoose.connect(
+//       "mongodb+srv://user:1234@cluster0.1tll05y.mongodb.net/item?retryWrites=true&w=majority"
+//     );
+//     mongoose.connection.on("connected", () => {
+//       console.log("connected to mongo");
+//     });
+//     app.listen(PORTLISTEN, () => {
+//       console.log("Application listening on port 3333!");
+//     });
+//     server.listen(port, function () {
+//       console.log(`MQTT Broker started on port ${port}`);
+//     });
+//     httpServer.listen(portMQ, function () {
+//       console.log("websocket server listening on port ", portMQ);
+//     });
+//     httpServer.on;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+// app.get("/", async (req, res) => {
+//   res.send("test");
+// });
+// app.get("/getItems", async (req, res) => {
+//   console.log(res);
+//   const items = await Item.find({});
+//   res.send(items);
+// });
+// start();
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("ws");
+const PORT = process.env.PORT || 3000;
+
+const wss = new Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
 });
 
-client.on("connect", () => {
-  client.subscribe(topic);
+app.get("/", (req, res) => {
+  res.send(`<h1>lsdkfdek ${PORT}</h1> `);
 });
-const wss = new ws.Server(
-  {
-    port: 8000,
-  },
-  () => console.log("serverStarted")
-);
-wss.on("connection", (ws) => {
-  ws.on("message", (message) => {
-    message = JSON.parse(message);
-    console.log(message);
-    wss.clients.forEach((client) => {
-      client.send(
-        JSON.stringify({
-          ...message,
-          new: true,
-          id: Date.now(),
-          time: Date.now(),
-        })
-      );
-    });
-  });
+
+server.listen(PORT, () => {
+  console.log("listening on *:3000");
 });
-const start = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://user:1234@cluster0.1tll05y.mongodb.net/item?retryWrites=true&w=majority"
-    );
-    mongoose.connection.on("connected", () => {
-      console.log("connected to mongo");
-    });
-    app.listen(PORTLISTEN, () => {
-      console.log("Application listening on port 3333!");
-    });
-    server.listen(port, function () {
-      console.log(`MQTT Broker started on port ${port}`);
-    });
-    httpServer.listen(portMQ, function () {
-      console.log("websocket server listening on port ", portMQ);
-    });
-    httpServer.on;
-  } catch (e) {
-    console.log(e);
-  }
-};
-app.get("/", async (req, res) => {
-  res.send("test");
-});
-app.get("/getItems", async (req, res) => {
-  console.log(res);
-  const items = await Item.find({});
-  res.send(items);
-});
-start();
