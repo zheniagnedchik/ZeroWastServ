95ed587203680ee9d9984b02b140e0f3c0eaa40b
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2022-10-13 05:17:59 -0500

    add

--- models/Item.js
+++ models/Item.js
@@ -2,6 +2,7 @@ const { Schema, model, ObjectId } = require("mongoose");
 
 const Item = new Schema({
   item: { type: Array },
+  weight: { type: Number },
 });
 
 module.exports = model("Item", Item);
